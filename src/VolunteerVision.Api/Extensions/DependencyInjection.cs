fc1e3e611be873d67ef640a6322f958c42230b05
Pedro Netto <pedronetto31415@gmail.com>
2024-09-19 11:29:34 -0300

    refactor(Presentation): code style in DI file

--- src/VolunteerVision.Api/Extensions/DependencyInjection.cs
+++ src/VolunteerVision.Api/Extensions/DependencyInjection.cs
@@ -1,18 +1,37 @@
+using System.Text.Json;
 using VolunteerVision.Api.Options;
 
 namespace VolunteerVision.Api.Extensions;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(
-        this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
     {
-        services
-            .AddOptions()
-            .ConfigureOptions<SwaggerOptionsSetup>()
-            .AddSwaggerGen()
-            .AddControllers();
+        AddControllers(services);
+        AddSwagger(services);
 
         return services;
+    }
+
+    private static void AddSwagger(IServiceCollection services)
+    {
+        services.AddSwaggerGen(config =>
+        {
+        });
+
+        services
+         .AddOptions()
+         .ConfigureOptions<SwaggerOptionsSetup>();
+
+    }
+
+    private static void AddControllers(IServiceCollection services)
+    {
+        var builder = services.AddControllers();
+
+        builder.AddJsonOptions(config =>
+        {
+            config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+        });
     }
 }
